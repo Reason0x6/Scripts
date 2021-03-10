850bd671d86c9a05feb2eee0b95bb20ee7e2ec3c
Gavin Austin <37852683+vk2fgav@users.noreply.github.com>
2021-03-10 18:06:40 +1100

    Added comments

--- DownloadsFiling/downloadFiling.ps1
+++ DownloadsFiling/downloadFiling.ps1
@@ -2,6 +2,8 @@
 $Location = "C:\Users\Reason6\Downloads"
 $MOVEDCOUNT = 0
 $FILESMOVED = @()
+
+#Finding Files & Creating days sort folder
 $files = Get-ChildItem -Path $Location -Attributes a
 $date = Get-Date -Format "yyy_MM_dd"
 
@@ -11,6 +13,8 @@ if(Test-Path "C:\Users\Reason6\Downloads\$date"){
     [system.io.directory]::CreateDirectory("C:\Users\Reason6\Downloads\$date")
 }
 
+
+## File Sorting
 $MOVEDCOUNT = 0
 foreach($file in $files){
     $ext = $file.Extension
@@ -24,11 +28,13 @@ foreach($file in $files){
     
     Move-Item -Path "C:\Users\Reason6\Downloads\$($file.Name)" -Destination "C:\Users\Reason6\Downloads\$date\$ext\$($file.Name)"
 
+    #logging data for this file
     $MOVEDCOUNT += 1
     $TEMPARR = @("$($file.Name)`n")
     $FILESMOVED += $TEMPARR
 }
 
+# Entering file data into log file
 $TimeStamp = Get-Date
 "$TimeStamp -> $MOVEDCOUNT Files Moved:`n $FILESMOVED" >> "C:\Users\Reason6\Downloads\Logs\Updates.txt"
 
